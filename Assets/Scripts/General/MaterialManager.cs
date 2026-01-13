f7b71a675c229e4363f6103a37659e4932495c73
MohammadShah406 <mianmohammadshah@gmail.com>
2026-01-13 14:04:06 +0000

    Add Aisha character assets and materials
    Introduced new Aisha character art assets, including FBX models, materials, textures for both 'Poor' and 'Rich' variants, and associated prefab files.
    Also added a new LightHolder prefab and updated a house material. These additions support the integration of the Aisha character into the project.

---
+++ Assets/Scripts/General/MaterialManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MaterialManager : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class MaterialSettings
+{
+    public List<Material> material;
+    public Color color;
+}
