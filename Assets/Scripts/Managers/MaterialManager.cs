9bc8580d3b9610a81cbfc5d4d47db11bee26fc59
MohammadShah406 <mianmohammadshah@gmail.com>
2026-02-24 23:31:44 +0000

    Add character-specific material upgrade support
    Introduce separate material lists and object lists for Bashir, Sahil and Aisha alongside existing house materials. Populate old material settings in Start for each character group, add per-character UpgradeXMaterials methods and batch UpgradeAllXMaterials calls when upgradeAll is triggered. Also add inspector headers and minor formatting/import adjustments. Prefab updated to include the new material entries and object placeholders.

--- Assets/Scripts/Managers/MaterialManager.cs
+++ Assets/Scripts/Managers/MaterialManager.cs
@@ -1,15 +1,38 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using System;
 
 public class MaterialManager : MonoBehaviour
 {
     public static MaterialManager instance;
-
+    [Header("House Materials")]
     public List<MaterialSettings> OldMaterialSettings;
     public List<MaterialSettings> upgradedMaterialSettings;
 
+    [Header("Bashir Materials")]
+    public List<MaterialSettings> oldBashirMaterialsSettings;
+    public List<MaterialSettings> upgradedBashirMaterialsSettings;
+
+    [Header("Sahil Materials")]
+    public List<MaterialSettings> oldSahilMaterialSettings;
+    public List<MaterialSettings> upgradedSahilMaterialSettings;
+
+    [Header("Aisha Materials")]
+    public List<MaterialSettings> oldAishaMaterialSettings;
+    public List<MaterialSettings> upgradedAishaMaterialSettings;
+
+
+    [Header("House Objects")]
     public List<GameObject> objectsToUpgrade;
+
+    [Header("Bashir Objects")]
+    public List<GameObject> bashirObjectsToUpgrade;
+    [Header("Sahil Objects")]
+    public List<GameObject> sahilObjectsToUpgrade;
+    [Header("Aisha Objects")]
+    public List<GameObject> aishaObjectsToUpgrade;
+
 
     public bool upgradedAll = false;
 
@@ -30,18 +53,68 @@ public class MaterialManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if(objectsToUpgrade.Count != upgradedMaterialSettings.Count)
+        if (objectsToUpgrade.Count != upgradedMaterialSettings.Count)
         {
             Debug.LogError("MaterialManager: The number of objects to upgrade does not match the number of material settings provided.");
         }
         else
         {
-            for(int i = 0; i < objectsToUpgrade.Count; i++)
+            for (int i = 0; i < objectsToUpgrade.Count; i++)
             {
                 OldMaterialSettings.Add(new MaterialSettings
                 {
                     name = objectsToUpgrade[i].name,
                     material = new List<Material>(objectsToUpgrade[i].GetComponent<Renderer>().materials)
+                });
+            }
+
+
+        }
+
+        if (bashirObjectsToUpgrade.Count != upgradedBashirMaterialsSettings.Count)
+        {
+            Debug.LogError("MaterialManager: The number of Bashir objects to upgrade does not match the number of Bashir material settings provided.");
+        }
+        else
+        {
+            for (int i = 0; i < bashirObjectsToUpgrade.Count; i++)
+            {
+                oldBashirMaterialsSettings.Add(new MaterialSettings
+                {
+                    name = bashirObjectsToUpgrade[i].name,
+                    material = new List<Material>(bashirObjectsToUpgrade[i].GetComponent<Renderer>().materials)
+                });
+            }
+        }
+
+        if (sahilObjectsToUpgrade.Count != upgradedSahilMaterialSettings.Count)
+        {
+            Debug.LogError("MaterialManager: The number of Sahil objects to upgrade does not match the number of Sahil material settings provided.");
+        }
+        else
+        {
+            for (int i = 0; i < sahilObjectsToUpgrade.Count; i++)
+            {
+                oldSahilMaterialSettings.Add(new MaterialSettings
+                {
+                    name = sahilObjectsToUpgrade[i].name,
+                    material = new List<Material>(sahilObjectsToUpgrade[i].GetComponent<Renderer>().materials)
+                });
+            }
+        }
+
+        if (aishaObjectsToUpgrade.Count != upgradedAishaMaterialSettings.Count)
+        {
+            Debug.LogError("MaterialManager: The number of Aisha objects to upgrade does not match the number of Aisha material settings provided.");
+        }
+        else
+        {
+            for (int i = 0; i < aishaObjectsToUpgrade.Count; i++)
+            {
+                oldAishaMaterialSettings.Add(new MaterialSettings
+                {
+                    name = aishaObjectsToUpgrade[i].name,
+                    material = new List<Material>(aishaObjectsToUpgrade[i].GetComponent<Renderer>().materials)
                 });
             }
         }
@@ -64,9 +137,38 @@ public class MaterialManager : MonoBehaviour
             {
                 upgradedAll = true;
                 UpgradeAllMaterials();
+
+                UpgradeAllBashirMaterials();
+                UpgradeAllSahilMaterials();
+                UpgradeAllAishaMaterials();
             }
         }
     }
+
+    private void UpgradeAllBashirMaterials()
+    {
+        for (int i = 0; i < bashirObjectsToUpgrade.Count; i++)
+        {
+            UpgradeBashirMaterials(i);
+        }
+    }
+
+    private void UpgradeAllSahilMaterials()
+    {
+        for (int i = 0; i < sahilObjectsToUpgrade.Count; i++)
+        {
+            UpgradeSahilMaterials(i);
+        }
+    }
+
+    private void UpgradeAllAishaMaterials()
+    {
+        for (int i = 0; i < aishaObjectsToUpgrade.Count; i++)
+        {
+            UpgradeAishaMaterials(i);
+        }
+    }
+
 
     public void UpgradeMaterials(int i)
     {
@@ -79,6 +181,21 @@ public class MaterialManager : MonoBehaviour
         {
             UpgradeMaterials(i);
         }
+    }
+
+    public void UpgradeBashirMaterials(int i)
+    {
+        bashirObjectsToUpgrade[i].GetComponent<Renderer>().materials = upgradedBashirMaterialsSettings[i].material.ToArray();
+    }
+
+    public void UpgradeSahilMaterials(int i)
+    {
+        sahilObjectsToUpgrade[i].GetComponent<Renderer>().materials = upgradedSahilMaterialSettings[i].material.ToArray();
+    }
+
+    public void UpgradeAishaMaterials(int i)
+    {
+        aishaObjectsToUpgrade[i].GetComponent<Renderer>().materials = upgradedAishaMaterialSettings[i].material.ToArray();
     }
 }
 
